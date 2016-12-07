3edf0a37e64bd00adf46438a623586400ddebd6e
Junnie-Jobs <dj0999@naver.com>
2016-12-07 18:57:35 +0900

    LectureExcerpt update

--- src/main/java/org/nhnnext/domain/LectureExcerpt.java
+++ src/main/java/org/nhnnext/domain/LectureExcerpt.java
@@ -1,13 +1,12 @@
 package org.nhnnext.domain;
 
-import org.springframework.data.rest.core.config.Projection;
-
 import java.util.List;
+import org.springframework.data.rest.core.config.Projection;
 
 @Projection(name = "excerpt", types = Lecture.class)
 public interface LectureExcerpt {
 
 	String getTitle();
 
-	List<Lesson> getSessions();
+	List<Lesson> getLessions();
 }

005c55763acf09129f91420f133d5739204011f3
Junnie-Jobs <dj0999@naver.com>
2016-12-07 18:56:03 +0900

    lesson domain update

--- src/main/java/org/nhnnext/domain/Lesson.java
+++ src/main/java/org/nhnnext/domain/Lesson.java
@@ -1,15 +1,12 @@
 package org.nhnnext.domain;
 
-import lombok.Data;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+
 import org.hibernate.validator.constraints.NotEmpty;
 import org.springframework.data.jpa.domain.AbstractPersistable;
 
-import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.OrderColumn;
-import javax.validation.constraints.NotNull;
-import java.util.List;
+import lombok.Data;
 
 @Data
 @Entity

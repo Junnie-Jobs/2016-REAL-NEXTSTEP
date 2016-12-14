84821d0cef476847ca3f6ff4c859971841f278b6
Junnie-Jobs <dj0999@naver.com>
2016-12-14 15:23:19 +0900

    Instructor update

--- src/main/java/org/nhnnext/nextstep/course/CourseExcerpt.java
+++ src/main/java/org/nhnnext/nextstep/course/CourseExcerpt.java
@@ -4,6 +4,7 @@ import java.util.List;
 import org.nhnnext.nextstep.session.CourseSession;
 import org.nhnnext.nextstep.session.CourseSessionExcerpt;
 import org.nhnnext.nextstep.session.MasterSession;
+import org.nhnnext.nextstep.user.Instructor;
 import org.nhnnext.nextstep.user.UserExcerpt;
 import org.springframework.data.rest.core.config.Projection;
 
@@ -13,7 +14,7 @@ public interface CourseExcerpt {
 	Long getId();
 	String getName();
 	String getDescription();
-	List<UserExcerpt> getInstructors();
+	List<Instructor> getInstructors();
 	MasterSession getMasterSession();	
 	List<CourseSessionExcerpt> getSessions();
 	CourseSession getDefaultSession();
--- src/main/java/org/nhnnext/nextstep/user/Instructor.java
+++ src/main/java/org/nhnnext/nextstep/user/Instructor.java
@@ -1,16 +1,10 @@
 package org.nhnnext.nextstep.user;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import org.nhnnext.nextstep.course.Course;
-
-import javax.persistence.Column;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
-import javax.persistence.ManyToMany;
-import java.util.ArrayList;
-import java.util.List;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(force = true)
 @Data
@@ -24,8 +18,8 @@ public class Instructor extends User {
 
 
 	//	@Column(unique = true)
-	@ManyToMany(mappedBy = "instructors")
-	private final List<Course> courses = new ArrayList<>();
+//	@ManyToMany(mappedBy = "instructors")
+//	private final List<Course> courses = new ArrayList<>();
 
 	// private final List<Session> sessions = new ArrayList<>();
 }

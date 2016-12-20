2f6e78791aa8792bc9acab4efd1ab0f713f4df07
Junnie-Jobs <dj0999@naver.com>
2016-12-20 20:30:25 +0900

    webconfig update

--- src/main/java/org/nhnnext/nextstep/config/WebSecurityConfig.java
+++ src/main/java/org/nhnnext/nextstep/config/WebSecurityConfig.java
@@ -1,18 +1,86 @@
 package org.nhnnext.nextstep.config;
 
+import org.nhnnext.nextstep.core.ClientResources;
+import org.nhnnext.nextstep.core.HttpStatusSuccessHandler;
+import org.nhnnext.nextstep.core.MyAuthorizationCodeAccessTokenProvider;
 import org.nhnnext.nextstep.user.security.SpringDataJpaUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
+import org.springframework.security.oauth2.client.token.AccessTokenProvider;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.filter.CompositeFilter;
+import javax.servlet.Filter;
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
-@EnableOAuth2Sso
+//@EnableOAuth2Sso
+@EnableOAuth2Client
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private ApplicationContext applicationContext;
+
+	@Autowired
+	private OAuth2ClientContext oauth2ClientContext;
+
+	@Bean
+	public FilterRegistrationBean oauth2ClientFilterRegistration(OAuth2ClientContextFilter filter) {
+		FilterRegistrationBean registration = new FilterRegistrationBean();
+		registration.setFilter(filter);
+		registration.setOrder(-100);
+		return registration;
+	}
+
+	@Bean
+	@ConfigurationProperties("github")
+	public ClientResources github() {
+		return new ClientResources();
+	}
+
+	private Filter ssoFilter() {
+		CompositeFilter filter = new CompositeFilter();
+		List<Filter> filters = new ArrayList<>();
+		filters.add(ssoFilter(github(), "/login/github"));
+		filter.setFilters(filters);
+		return filter;
+	}
+
+	private Filter ssoFilter(ClientResources client, String path) {
+		OAuth2RestTemplate restTemplate = new OAuth2RestTemplate(client.getClient(), oauth2ClientContext);
+		AccessTokenProvider accessTokenProvider = new MyAuthorizationCodeAccessTokenProvider();
+		restTemplate.setAccessTokenProvider(accessTokenProvider);
+
+		UserInfoTokenServices tokenServices = new UserInfoTokenServices(client.getResource().getUserInfoUri(), client.getClient().getClientId());
+		tokenServices.setRestTemplate(restTemplate);
+
+		OAuth2ClientAuthenticationProcessingFilter filter = new OAuth2ClientAuthenticationProcessingFilter(path);
+		filter.setRestTemplate(restTemplate);
+		filter.setTokenServices(tokenServices);
+		filter.setApplicationEventPublisher(this.applicationContext);
+		filter.setAuthenticationSuccessHandler(new HttpStatusSuccessHandler(HttpStatus.NO_CONTENT));
+		return filter;
+	}
+
+
 
 	@Autowired
 	private SpringDataJpaUserDetailsService userDetailsService;
@@ -61,7 +129,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf()
                     .disable()
                 .headers()
-                    .frameOptions().disable();
+                    .frameOptions().disable().and()
+				.exceptionHandling()
+//					.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+					.defaultAuthenticationEntryPointFor(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED), new AntPathRequestMatcher("/api/**"))
+					.and()
+//				.formLogin()
+//					.successHandler(new MyAuthenticationSuccessHandler())
+//					.and()
+				.addFilterBefore(ssoFilter(), BasicAuthenticationFilter.class);
+//				.authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/login"));
         // @formatter:on
 	}
 

9c6b25d62cd9da1a6b1ada9c8e4d362cedb83c27
Junnie-Jobs <dj0999@naver.com>
2016-12-20 16:42:13 +0900

    redid update

--- src/main/java/org/nhnnext/nextstep/repository/CourseSessionRepository.java
+++
@@ -1,8 +0,0 @@
-package org.nhnnext.nextstep.session;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource(excerptProjection = CourseSessionExcerpt.class)
-public interface CourseSessionRepository extends CrudRepository<CourseSession, Long> {
-}

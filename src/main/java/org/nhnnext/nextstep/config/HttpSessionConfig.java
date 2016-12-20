cd70b8236aad54b996c14536c15b78f167eeb689
Junnie-Jobs <dj0999@naver.com>
2016-12-20 16:24:48 +0900

    redis config setting

--- src/main/java/org/nhnnext/nextstep/config/HttpSessionConfig.java
+++ src/main/java/org/nhnnext/nextstep/config/HttpSessionConfig.java
@@ -1,16 +1,16 @@
 package org.nhnnext.nextstep.config;
 
-//@Configuration
-//@EnableRedisHttpSession
-//public class HttpSessionConfig {
-//
-//    @Bean
-//    public LettuceConnectionFactory connectionFactory() {
-//        return new LettuceConnectionFactory();
-//    }
-//
-//    @Bean
-//    public HttpSessionStrategy httpSessionStrategy() {
-//        return new HeaderHttpSessionStrategy();
-//    }
-//}
+@Configuration
+@EnableRedisHttpSession
+public class HttpSessionConfig {
+
+    @Bean
+    public LettuceConnectionFactory connectionFactory() {
+        return new LettuceConnectionFactory();
+    }
+
+    @Bean
+    public HttpSessionStrategy httpSessionStrategy() {
+        return new HeaderHttpSessionStrategy();
+    }
+}

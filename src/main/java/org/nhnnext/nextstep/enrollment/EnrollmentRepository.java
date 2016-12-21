42a274dfe065b6f1b82ff30e02c2c341bf01f564
Junnie-Jobs <dj0999@naver.com>
2016-12-21 20:02:46 +0900

    refactoring

---
+++ src/main/java/org/nhnnext/nextstep/enrollment/EnrollmentRepository.java
@@ -0,0 +1,9 @@
+package org.nhnnext.nextstep.enrollment;
+
+import org.nhnnext.nextstep.core.repository.AuditingRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(excerptProjection = EnrollmentExcerpt.class)
+public interface EnrollmentRepository extends AuditingRepository<Enrollment, Long> {
+}
---
+++ src/main/java/org/nhnnext/nextstep/enrollment/EnrollmentService.java
@@ -0,0 +1,11 @@
+package org.nhnnext.nextstep.enrollment;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EnrollmentService {
+
+//    public Enrollment enroll(CourseSession session) {
+//
+//    }
+}

2453fff6915cebb1d2acbc23522de9b2cd48d42a
Junnie-Jobs <dj0999@naver.com>
2016-12-21 14:13:20 +0900

    unused import delete

--- src/main/java/org/nhnnext/nextstep/session/CourseSession.java
+++ src/main/java/org/nhnnext/nextstep/session/CourseSession.java
@@ -1,22 +1,11 @@
 package org.nhnnext.nextstep.session;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
-
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
-
-import org.hibernate.validator.constraints.NotEmpty;
-import org.nhnnext.nextstep.course.Course;
 import org.nhnnext.nextstep.enrollment.Enrollment;
-import org.nhnnext.nextstep.lecture.Lecture;
-import org.nhnnext.nextstep.user.User;
-
 import lombok.Data;
 import lombok.NoArgsConstructor;
 

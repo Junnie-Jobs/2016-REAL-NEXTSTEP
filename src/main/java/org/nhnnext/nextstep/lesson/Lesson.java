598a5a575d79de4d715c3eae618fe9657ff9f1c9
Junnie-Jobs <dj0999@naver.com>
2016-12-21 20:58:59 +0900

    update

--- src/main/java/org/nhnnext/nextstep/lesson/Lesson.java
+++ src/main/java/org/nhnnext/nextstep/lesson/Lesson.java
@@ -6,6 +6,7 @@ import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotNull;
 import org.hibernate.validator.constraints.NotEmpty;
 import org.nhnnext.nextstep.core.domain.AbstractEntity;
+import org.nhnnext.nextstep.course.domain.AbstractCourseEntity;
 import org.nhnnext.nextstep.lecture.Lecture;
 import org.springframework.security.core.Authentication;
 import org.springframework.util.Assert;
@@ -14,7 +15,7 @@ import lombok.Data;
 
 @Data
 @Entity
-public class Lesson extends AbstractEntity {
+public class Lesson extends AbstractCourseEntity {
 
     @NotNull
     private Access access = Access.PRIVATE;

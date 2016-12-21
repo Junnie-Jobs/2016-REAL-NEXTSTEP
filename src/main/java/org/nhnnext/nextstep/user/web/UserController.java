f41f1e48c17632cbc91e78cca7c5fcababea6ff5
Junnie-Jobs <dj0999@naver.com>
2016-12-21 14:27:56 +0900

    user Controller update

--- src/main/java/org/nhnnext/nextstep/user/web/UserController.java
+++ src/main/java/org/nhnnext/nextstep/user/web/UserController.java
@@ -1,6 +1,8 @@
 package org.nhnnext.nextstep.user.web;
 
 import java.security.Principal;
+
+import org.nhnnext.nextstep.user.User;
 import org.nhnnext.nextstep.user.UserService;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,15 +15,15 @@ public class UserController {
 
     private final UserService service;
 
-//    @PreAuthorize("isAuthenticated()")
-//    @GetMapping("/api/user")
-//    public User getAuthenticatedUser() {
-//        return service.getAuthenticatedUser().orElseGet(null);
-//    }
-
     @PreAuthorize("isAuthenticated()")
     @GetMapping("/api/user")
-    public Principal user(Principal principal) {
-        return principal;
+    public User getAuthenticatedUser() {
+        return service.getAuthenticatedUser().orElseGet(null);
     }
+
+//    @PreAuthorize("isAuthenticated()")
+//    @GetMapping("/api/user")
+//    public Principal user(Principal principal) {
+//        return principal;
+//    }
 }

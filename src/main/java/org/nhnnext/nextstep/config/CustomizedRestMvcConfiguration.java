db7a33c24365ecd70587b6c9767a344bf3c462de
Junnie-Jobs <dj0999@naver.com>
2016-12-21 18:49:40 +0900

    configuration update

--- src/main/java/org/nhnnext/nextstep/config/CustomizedRestMvcConfiguration.java
+++ src/main/java/org/nhnnext/nextstep/config/CustomizedRestMvcConfiguration.java
@@ -2,6 +2,9 @@ package org.nhnnext.nextstep.config;
 
 import org.nhnnext.nextstep.course.Course;
 import org.nhnnext.nextstep.lecture.Lecture;
+import org.nhnnext.nextstep.lesson.Lesson;
+import org.nhnnext.nextstep.session.CourseSession;
+import org.nhnnext.nextstep.session.MasterSession;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
 import org.springframework.stereotype.Component;
@@ -12,6 +15,9 @@ public class CustomizedRestMvcConfiguration extends RepositoryRestConfigurerAdap
 	 @Override
 	  public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
 		 config.exposeIdsFor(Course.class);
+		 config.exposeIdsFor(CourseSession.class);
+		 config.exposeIdsFor(MasterSession.class);
+		 config.exposeIdsFor(Lesson.class);
 		 config.exposeIdsFor(Lecture.class);
 	  }
 }

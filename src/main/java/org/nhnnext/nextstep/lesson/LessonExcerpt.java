ae8bc9fea1c6b5ae10829e1f87522367ba827950
Junnie-Jobs <dj0999@naver.com>
2017-01-11 20:31:58 +0900

    update

--- src/main/java/org/nhnnext/nextstep/lesson/LessonExcerpt.java
+++ src/main/java/org/nhnnext/nextstep/lesson/LessonExcerpt.java
@@ -1,11 +1,30 @@
 package org.nhnnext.nextstep.lesson;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import org.nhnnext.nextstep.discussion.Discussion;
+import org.nhnnext.nextstep.lecture.Lecture;
+import org.nhnnext.nextstep.lesson.Lesson.Access;
 import org.springframework.data.rest.core.config.Projection;
 
 @Projection(name = "excerpt", types = Lesson.class)
 public interface LessonExcerpt {
 
     Long getId();
+    
+    String getContent();
+    
+    Lecture getLecture();
 
     String getName();
+    
+    Access getAcess();
+
+    List<Discussion> getDiscussions();
+
 }
